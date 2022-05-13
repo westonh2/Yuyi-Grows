67153c3f7449925f4afb34b0d174d6820b1137d4
westonh2 <weston@full-circle.design>
2022-05-13 14:25:01 -0400

    .

--- js/index.js
+++ js/index.js
@@ -6,3 +6,19 @@ function classToggle() {
 
 document.querySelector('.nav-toggle')
   .addEventListener('click', classToggle);
+
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var contents = this.nextElementSibling;
+    if (contents.style.display === "block") {
+      contents.style.display = "none";
+    } else {
+      contents.style.display = "block";
+    }
+  });
+}
